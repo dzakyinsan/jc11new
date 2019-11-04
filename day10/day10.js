20a507b8ab34a1dab727cd3a94eea47d0865fce6
Dino <aldinorahman36@gmail.com>
2019-11-04 13:12:50 +0700

    day102

--- day10/day10.js
+++ day10/day10.js
@@ -84,3 +84,86 @@ var manusia = {
 var kelas='jc13'
 var namamurid=kelas=='jc11'?'farhan':kelas=='jc12'?'budi':'bambang'
 console.log(namamurid)
+
+
+// var a='hallo'
+// console.log()
+// var a=new Array(5)
+// // console.log(a)
+// for (let i=0;i<5;i++){
+//     if(i===3){
+//         continue
+//     }
+//     console.log(i)
+//
+class Manusia {
+    constructor(nama,pekerjaan,umur){
+        // console.log(nama)
+        this.nama=nama
+        this.pekerjaan=pekerjaan
+        this.umur=umur
+    }
+    get kalimat(){
+        console.log(`${this.nama} bekerja sebagai ${this.pekerjaan}, dan berumur ${this.umur} `)
+    }
+
+}
+// console.log(new kelas("joey","actor",14))
+// var arr=[]
+// arr.push(new Manusia("joey","actor",31))
+// console.log(new Manusia("joey","actor",31).kalimat)
+// new Manusia("joey","actor",31).kalimat
+
+class Persegi{
+    constructor(sisi){
+        this.panjang=sisi
+        this.lebar=sisi
+        this.luaspersegi=function(){
+            return this.panjang*this.lebar
+        }
+    }
+    get luas (){
+        return this.panjang*this.lebar
+    }
+    set luas(a){
+        this.panjang=Math.sqrt(a)
+        this.lebar=Math.sqrt(a)
+    }
+    static inipersegi(Sisi1,Sisi2){
+        return Sisi1===Sisi2?"ini persegi":"ini bukan persegi"
+    }
+}
+// let persegi1=new Persegi(4)
+// console.log(persegi1.luaspersegi())
+// console.log(persegi1.luas)
+// persegi1.luas=36
+// console.log(persegi1.inipersegi(2,3))
+// console.log(Persegi.inipersegi(12,13))
+
+class Orang{
+    constructor(nama,b){
+        this.nama=nama
+        this.usia=b
+    }
+    deskripsi(){
+        console.log(`nama ${this.nama}, usia saya ${this.usia}`)
+    }
+}
+class Programmer extends Orang{
+    constructor(nama,usia,pengalaman){
+        super(nama,usia)
+        //super(nama,usia)//arus urutan sesuai dengan yang diextend
+        this.pengalaman=pengalaman
+    }
+    code(){
+        console.log(`nama ${this.nama}, usia saya ${this.usia} dan pengalaman ngoding${this.pengalaman}`)
+    }
+    deskrip(){
+        super.deskripsi()
+        console.log('ini deskrip')
+    }
+    
+}
+console.log(new Programmer('dino',22,2).deskripsi())
+// var j='dadas'
+// console.log(Math.round(4.7))
